df6b3541493f826e25a8c4e27b1eb180da96cf74
ASHISHVERMASH <160701608+ASHISHVERMASH@users.noreply.github.com>
2025-03-14 01:24:51 +0530

    java practice

---
+++ src/basic/CaseCheck.java
@@ -0,0 +1,17 @@
+package basic;
+
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner in  = new Scanner(System.in);
+        //  char ch = in.next().trim().charAt(0);
+        String word = "hello";
+        System.out.println(word.charAt(2));
+
+        System.out.println(in.next().trim());
+
+    }
+}
+
+
