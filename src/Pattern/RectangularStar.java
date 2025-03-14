bb7ea248c677faba3793436a5d81b7148bc6be59
ASHISHVERMASH <160701608+ASHISHVERMASH@users.noreply.github.com>
2025-03-14 18:09:50 +0530

    Rectangular Star Pattern

---
+++ src/Pattern/RectangularStar.java
@@ -0,0 +1,16 @@
+package Pattern;
+import java.util.Scanner;
+
+public class RectangularStar {
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        for (int i =0;i<N;i++) {
+            for(int j=0;j<N;j++ ) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
